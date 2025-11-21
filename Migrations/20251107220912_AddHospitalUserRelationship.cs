5da4b419e5a5e6b72681e767de227f5b8404a2ff
CalebBav234 <calebmbaving@gmail.com>
2025-11-20 20:41:07 -0400

    Feat: Caleb's part merged with main

---
+++ Migrations/20251107220912_AddHospitalUserRelationship.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Security.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHospitalUserRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "doctors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Speciality = table.Column<string>(type: "text", nullable: false),
+                    HospitalId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_doctors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hospitals",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    AdminId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hospitals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false),
+                    RefreshToken = table.Column<string>(type: "text", nullable: true),
+                    RefreshTokenExpiresAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    RefreshTokenRevokedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CurrentJwtId = table.Column<string>(type: "text", nullable: true),
+                    hospitalId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Hospitals_hospitalId",
+                        column: x => x.hospitalId,
+                        principalTable: "Hospitals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hospitals_AdminId",
+                table: "Hospitals",
+                column: "AdminId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_hospitalId",
+                table: "Users",
+                column: "hospitalId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hospitals_Users_AdminId",
+                table: "Hospitals",
+                column: "AdminId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hospitals_Users_AdminId",
+                table: "Hospitals");
+
+            migrationBuilder.DropTable(
+                name: "doctors");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Hospitals");
+        }
+    }
+}
---
+++ Migrations/20251108132858_MakeHospitalIdRequired.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Security.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeHospitalIdRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "UPDATE \"Users\" SET \"hospitalId\" = 'a3b0a700-879e-4bcc-9865-a011b583c087' WHERE \"hospitalId\" IS NULL;"
+            );
+             // Now enforce NOT NULL without default
+            migrationBuilder.AlterColumn<Guid>(
+                name: "hospitalId",
+                table: "Users",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "hospitalId",
+                table: "Users",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+    }
+}
